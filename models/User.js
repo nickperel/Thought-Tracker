62f3ef91aa360ad98fca15905fa351961c119ad2
Kakashi <nickperel@live.com>
2022-02-25 17:56:38 -0500

    initial challenge setup with folder structure and created the User model

---
+++ models/User.js
@@ -0,0 +1,48 @@
+const { Schema, model } = require('mongoose');
+
+const UserSchema = new Schema(
+    {
+      username: {
+        type: String,
+        unique: true,
+        required: true,
+        trim: true
+      },
+      email: {
+        type: String,
+        unique: true,
+        required: true,
+        match: /.+\@.+\..+/
+      },
+      thoughts: [
+          {
+            type: Schema.Types.ObjectId,
+            ref: 'Thought'
+          }
+        ],
+      friends: [
+        {
+          type: Schema.Types.ObjectId,
+          ref: 'User'
+        }
+      ],
+    },
+    {
+      toJSON: {
+        virtuals: true,
+        getters: true
+      },
+      id: false
+    }
+  );
+
+// get total count of comments and replies on retrieval
+UserSchema.virtual('friendCount').get(function() {
+    return this.friends.length;
+  });
+
+  // create the Pizza model using the PizzaSchema
+const User = model('User', UserSchema);
+
+// export the Pizza model
+module.exports = User;
